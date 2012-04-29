16b79508cee1fca9244017c2841d6ced7a9c4970
Abhimanyu Saxena <abhimanyusaxena@gmail.com>
2012-04-29 23:26:43 +0530

    init rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Picshub::Application.config.secret_token = 'ce778c50c4b5a53551b0200e5b7e202db92608d8049a6c8240edd3c33bf542dca3b417beb124c8fc3ff85f436edc32785394e10fbbfc15699c739e830b50b92d'
