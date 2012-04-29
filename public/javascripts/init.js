38b35968014e8a745254f5756c1f8793f82c6fa5
Abhimanyu Saxena <abhimanyusaxena@gmail.com>
2012-04-30 00:38:42 +0530

    backbone structure

---
+++ public/javascripts/init.js
@@ -0,0 +1,28 @@
+/*****************Model Definitions*******************/
+var Picture = Backbone.Model.extend({
+    initialize : function(config){
+        
+    }
+});
+
+/*******************Collection Definitins********************/
+var PictureCollection = Backbone.Collection.extend({
+    initialize : function(config){
+        
+    }
+});
+
+
+/*******************Views Definitions*************************/
+var TabPanel = Backbone.View.extend({
+    
+});
+
+var PictureWall = Backbone.View.extend({
+    
+});
+
+var PictureTile = Backbone.View.extend({
+    
+});
+
