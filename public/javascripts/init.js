beabd72cd1f4db63641d3c6346f83ae728117efa
Abhimanyu Saxena <abhimanyusaxena@gmail.com>
2012-06-05 00:12:48 +0530

    wishlist feature

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,5 +1,29 @@
-//"use strict"
 
+//"use strict"
+$.Mason.prototype.resize = function() {
+	this._getColumns();
+	this._reLayout();
+};
+$.Mason.prototype._reLayout = function( callback ) {
+	var freeCols = this.cols;
+	if ( this.options.cornerStampSelector ) {
+		var $cornerStamp = this.element.find( this.options.cornerStampSelector ),
+		cornerStampX = $cornerStamp.offset().left -
+		( this.element.offset().left + this.offset.x + parseInt($cornerStamp.css('marginLeft')) );
+		freeCols = Math.floor( cornerStampX / this.columnWidth );
+	}
+	// reset columns
+	var i = this.cols;
+	this.colYs = [];
+	while (i--) {
+		this.colYs.push( this.offset.y );
+	}
+	for ( i = freeCols; i < this.cols; i++ ) {
+		this.colYs[i] = this.offset.y + $cornerStamp.outerHeight(true);
+	}
+	// apply layout logic to all bricks
+	this.layout( this.$bricks, callback );
+}; 
 /*****************Model Definitions*******************/
 var Picture = Backbone.Model.extend({
 	initialize : function(config){
@@ -40,102 +64,102 @@ var Header = Backbone.View.extend({
 		this.loggedInTemplate = _.template(['<div class="navigation sticky">',
 			'<h1 class="logo floatLeft"><a href="#">XXX</a></h1>',
 			'<ul class="navigationLinks floatRight">',
-				'<li class="recentPic">',
-					'<a href="">PHOTOS </a>',
-					'<div class="subNav displayNone subNavFirst">',
-						'<ul>',
-							'<li>',
-								'<a href="">Most Popular Photos</a>',
-							'</li>',
-							'<li>',
-								'<a href="">Most Recent Photos</a>',
-							'</li>',
-							'<li>',
-								'<a href="">Photos Of The Day</a>',
-							'</li>',
-							'<li class="lastLi">',
-								'<a href="">Photos Of The Week</a>',
-							'</li>',
-						'</ul>',
-					'</div>',
-				'</li>',
-				'<li class="recentVideo">',
-					'<a href="">VIDEOS </a>',
-					'<div class="subNav displayNone subNavSecond">',
-						'<ul>',
-							'<li>',
-								'<a href="">Most Popular Videos</a>',
-							'</li>',
-							'<li>',
-								'<a href="">Most Recent Videos</a>',
-							'</li>',
-							'<li>',
-								'<a href="">Videos Of The Day</a>',
-							'</li>',
-							'<li  class="lastLi">',
-								'<a href="">Videos Of The Week</a>',
-							'</li>',
-						'</ul>',
-					'</div>',
-				'</li>',
-				'<li class="allPhotos">',
-					'<a href="">PHOTOS HUB </a>',
-					'<div class="subNav displayNone subNavThird">',
-						'<ul>',
-							'<li>',
-								'<a href="">Facebook</a>',
-							'</li>',
-							'<li>',
-								'<a href="">Instagram</a>',
-							'</li>',
-							'<li>',
-								'<a href="">Flicker</a>',
-							'</li>',
-							'<li>',
-								'<a hr	ef="">Picasa</a>',
-							'</li>',
-							'<li  class="lastLi">',
-								'<a href="">Google Plus</a>',
-							'</li>',
-						'</ul>',
-					'</div>',
-				'</li>',
-				'<li class="ownerDetails">',
-					'<div class="updatedBy box morphing-glowing floatLeft">',
-					'<a href="javascript:void(0);">',
-						'<span class="image-wrap " style="position:relative; display:inline-block; background:url(http://graph.facebook.com/<%=userID%>/picture) no-repeat center center; width: 35px; height: 35px;">',
-						'</span>',
-					'</a>',
-					'<div class="subNav displayNone  subNavForth" style="right: -2px;top: 32px;z-index: -1;">',
-						'<ul>',
-							'<li>',
-								'<a href="">Profile</a>',
-							'</li>',
-							'<li>',
-								'<a href="">Invite</a>',
-							'</li>',
-							'<li>',
-								'<a href="">Popular Photos</a>',
-							'</li>',
-							'<li>',
-								'<a href="">Popular Videos</a>',
-							'</li>',
-							'<li>',
-								'<a href="">Settings</a>',
-							'</li>',
-							'<li  class="lastLi">',
-								'<a href="">Logout</a>',
-							'</li>',
-						'</ul>',
-					'</div>',
-				'</div>',
+			'<li class="recentPic">',
+			'<a href="">PHOTOS </a>',
+			'<div class="subNav displayNone subNavFirst">',
+			'<ul>',
+			'<li>',
+			'<a href="">Most Popular Photos</a>',
 			'</li>',
-		'</ul>',
-		'</div>'].join(''));
+			'<li>',
+			'<a href="">Most Recent Photos</a>',
+			'</li>',
+			'<li>',
+			'<a href="">Photos Of The Day</a>',
+			'</li>',
+			'<li class="lastLi">',
+			'<a href="">Photos Of The Week</a>',
+			'</li>',
+			'</ul>',
+			'</div>',
+			'</li>',
+			'<li class="recentVideo">',
+			'<a href="">VIDEOS </a>',
+			'<div class="subNav displayNone subNavSecond">',
+			'<ul>',
+			'<li>',
+			'<a href="">Most Popular Videos</a>',
+			'</li>',
+			'<li>',
+			'<a href="">Most Recent Videos</a>',
+			'</li>',
+			'<li>',
+			'<a href="">Videos Of The Day</a>',
+			'</li>',
+			'<li  class="lastLi">',
+			'<a href="">Videos Of The Week</a>',
+			'</li>',
+			'</ul>',
+			'</div>',
+			'</li>',
+			'<li class="allPhotos">',
+			'<a href="">PHOTOS HUB </a>',
+			'<div class="subNav displayNone subNavThird">',
+			'<ul>',
+			'<li>',
+			'<a href="">Facebook</a>',
+			'</li>',
+			'<li>',
+			'<a href="">Instagram</a>',
+			'</li>',
+			'<li>',
+			'<a href="">Flicker</a>',
+			'</li>',
+			'<li>',
+			'<a hr	ef="">Picasa</a>',
+			'</li>',
+			'<li  class="lastLi">',
+			'<a href="">Google Plus</a>',
+			'</li>',
+			'</ul>',
+			'</div>',
+			'</li>',
+			'<li class="ownerDetails">',
+			'<div class="updatedBy box morphing-glowing floatLeft">',
+			'<a href="javascript:void(0);">',
+			'<span class="image-wrap " style="position:relative; display:inline-block; background:url(http://graph.facebook.com/<%=userID%>/picture) no-repeat center center; width: 35px; height: 35px;">',
+			'</span>',
+			'</a>',
+			'<div class="subNav displayNone  subNavForth" style="right: -2px;top: 32px;z-index: -1;">',
+			'<ul>',
+			'<li>',
+			'<a href="">Profile</a>',
+			'</li>',
+			'<li>',
+			'<a href="">Invite</a>',
+			'</li>',
+			'<li>',
+			'<a href="">Popular Photos</a>',
+			'</li>',
+			'<li>',
+			'<a href="">Popular Videos</a>',
+			'</li>',
+			'<li>',
+			'<a href="">Settings</a>',
+			'</li>',
+			'<li  class="lastLi">',
+			'<a href="">Logout</a>',
+			'</li>',
+			'</ul>',
+			'</div>',
+			'</div>',
+			'</li>',
+			'</ul>',
+			'</div>'].join(''));
 		this.loggedOutTemplate = _.template([
-		'<div class="navigation sticky">',
+			'<div class="navigation sticky">',
 			'<h1 class="logo floatLeft"><a href="#">XXX</a></h1>',
-		'</div>'].join(''));
+			'</div>'].join(''));
 	},
 	render : function(config){
 		if(config.user && config.user.isAuthed()){
@@ -177,6 +201,7 @@ var PictureWall = Backbone.View.extend({
 	initialize : function(){
 		hub.bind('authed',this.showCustomWall,this);
 		hub.bind('guestInit',this.showGuestWall,this);
+		
 		this.items = [];
 	},
 	render : function(){
@@ -191,28 +216,45 @@ var PictureWall = Backbone.View.extend({
 				that.populateWall(res);
 			},
 			error : function(){
-				//TODO show error message
+			//TODO show error message
 			}
 		});
 	},
 	populateWall : function(res){
 		var that = this;
 		setTimeout(function(){
-				$('.mainWrapper').activity(false);
-				app.getStore().reset(res.data);
-				for(var i=0,len=app.getStore().length;i<len;i++){
-					var item = new PictureTile(app.getStore().at(i));
-					item.render();
-					$('.mainWrapper').append(item.el);
-					that.items.push(item);
+			$('.mainWrapper').activity(false);
+			app.getStore().reset(res.data);
+			$('.mainWrapper').append("<div class='wishList'> Watched Items</div>");
+			$('.wishList').droppable({
+				drop : function(event,ui){
+					var item = $(ui.draggable).detach();
+					item.removeClass('tile');
+					item.css({
+						height: 64,
+						width : 64,
+						overflow : 'hidden'
+					})
+					$('.wishList').append(item);
+					$('.mainWrapper').masonry('reload');
 				}
-				$('.mainWrapper').imagesLoaded(function(){
-					$('.mainWrapper').masonry({
-						itemSelector : '.tile',
-						columnWidth : 270
-					});
+			});
+			for(var i=0,len=app.getStore().length;i<len;i++){
+				var item = new PictureTile(app.getStore().at(i));
+				item.render();
+				$('.mainWrapper').append(item.el);
+				that.items.push(item);
+			}
+			
+			$('.mainWrapper').imagesLoaded(function(){
+				
+				$('.mainWrapper').masonry({
+					itemSelector : '.tile',
+					columnWidth : 270,
+					cornerStampSelector: '.wishList'
 				});
-			},2000);
+			});
+		},20);
 	},
 	showGuestWall : function(){
 		var that = this;
@@ -223,7 +265,7 @@ var PictureWall = Backbone.View.extend({
 				that.populateWall(res);
 			},
 			error : function(){
-				//TODO show error message
+			//TODO show error message
 			}
 		});
 	}
@@ -236,66 +278,93 @@ var PictureTile = Backbone.View.extend({
 	initialize : function(config){
 		this.template = _.template([
 			'<div itemtype="javascript:void(0)" itemscope="" class="item photo">',
-        '<div class="modal-media wrapper cboxElement  view second-effect">',
-          '<a class="imgContent" href="#">',
-            '<img width="240" src="<%=imageUrl%>" alt="">',
-          '</a>',
+			'<div class="modal-media wrapper cboxElement  view second-effect">',
+			'<a class="imgContent" href="#">',
+			'<img width="240" src="<%=imageUrl%>" alt="">',
+			'</a>',
 
-          '<div class="mask">',
-            '<div class="itemDisc">',
-              '<span class="itemName">Arrow New York</span>',
-              '<span class="itemNameDesc">Men Check Navy Blue Shirt</span>',
-              '<div class="priceDetails">',
-                '<div class="floatLeft">',
-                  '<span class="discountedPrice red">Rs. 810 <span class="strike gray originalPrice">899</span></span>',
-                  '<div class="perOff red fontBold">(10% OFF)</div>',
-                '</div>',
-                '<div class="floatRight">',
-                  '<a class="grabIt " target="_self" href="#"><span class="left"> Grab It! </span></a>',
-                '</div>',
-              '</div>',
-            '</div>',
-            '<div class="socialToolbar socialLinks forLiveFeeds showtoolbar" style="">',
-              '<span class="floatLeft" style="margin-top: 8px;margin-left: 5px;background: green">',
-              '</span>',
-              '<span class="floatLeft flike" style="margin-top: 8px;margin-left: 5px;background: green"></span>',
-            '</div>',
-          '</div>',
+			'<div class="mask">',
+			'<div class="itemDisc">',
+			'<span class="itemName">Arrow New York</span>',
+			'<span class="itemNameDesc">Men Check Navy Blue Shirt</span>',
+			'<div class="priceDetails">',
+			'<div class="floatLeft">',
+			'<span class="discountedPrice red">Rs. 810 <span class="strike gray originalPrice">899</span></span>',
+			'<div class="perOff red fontBold">(10% OFF)</div>',
+			'</div>',
+			'<div class="floatRight">',
+			'<a class="grabIt " target="_self" href="#"><span class="left"> Grab It! </span></a>',
+			'</div>',
+			'</div>',
+			'</div>',
+			'<div class="socialToolbar socialLinks forLiveFeeds showtoolbar" style="">',
+			'<span class="floatLeft" style="margin-top: 8px;margin-left: 5px;background: green">',
+			'</span>',
+			'<span class="floatLeft flike" style="margin-top: 8px;margin-left: 5px;background: green"></span>',
+			'</div>',
+			'</div>',
 
-        '</div>',
-        '<div class="modal-media wrapper cboxElement">',
-          '<div class="typeInfo clear">',
-            '<div class="floatLeft">',
-              '<span class="updatedBy box morphing-glowing floatLeft"  style="width: 100%;">',
-                '<a href="javascript:void(0);">',
-                  '<span class="image-wrap " style="position:relative; display:inline-block; background:url(images/logo-myntra.png) no-repeat center center; background-size: 47px 55px;width: 40px; height: 40px;">',
-                    '<img width="40" height="40" src="images/logo-myntra.png" style="opacity: 0; ">',
-                  '</span>',
-                '</a>',
-                '<a href="javascript:void(0);" class="profilePic floatRight" style="margin: 10px 0 0 5px;"><span class="font11" style="color:#999">by</span> <span class="font12">Myntra</span></a>',
-              '</span>',
-            '</div>',
-            '<div class="floatRight font12" style="margin-top: 14px;">',
-              '<span class="howMayLicks color3 floatRight">',
-                '<span class="heartIconGray floatLeft" style="margin-right: 3px"></span>',
-                '<span class="favCount floatRight">12</span>',
-              '</span>',
-            '</div>',
-          '</div>',
-        '</div>',
-      '</div>'
-		].join(""));
+			'</div>',
+			'<div class="modal-media wrapper cboxElement">',
+			'<div class="typeInfo clear">',
+			'<div class="floatLeft">',
+			'<span class="updatedBy box morphing-glowing floatLeft"  style="width: 100%;">',
+			'<a href="javascript:void(0);">',
+			'<span class="image-wrap " style="position:relative; display:inline-block; background:url(<%=source.logo%>) no-repeat center center; background-size: 47px 55px;width: 40px; height: 40px;">',
+			'<img width="40" height="40" src="<%=source.logo%>" style="opacity: 0; ">',
+			'</span>',
+			'</a>',
+			'<a href="javascript:void(0);" class="profilePic floatRight" style="margin: 10px 0 0 5px;"><span class="font11" style="color:#999">by</span> <span class="font12"><%=source.name%></span></a>',
+			'</span>',
+			'</div>',
+			'<div class="floatRight font12" style="margin-top: 14px;">',
+			'<span class="howMayLicks color3 floatRight">',
+			'<span class="heartIconGray floatLeft" style="margin-right: 3px"></span>',
+			'<span class="favCount floatRight">12</span>',
+			'</span>',
+			'</div>',
+			'</div>',
+			'</div>',
+			'</div>'
+			].join(""));
 		this.model = config;
 		return this;
 	},
 	render : function(){
 		$(this.el).html(this.template(this.model.toJSON()));
+		this.addHandlers();
 		$(this.el).draggable({
 			stop : function(){
 				$('.mainWrapper').masonry('reload');
+				$(this).removeClass('noAnimation');
+			},
+			start : function(){
+				$(this).addClass('noAnimation');
 			}
 		});
 		return this;
+	},
+	addHandlers : function(){
+		var that = this;
+		$(".grabIt ",this.el).click(function(){
+			that.grabIt();
+		});
+	},
+	grabIt : function(){
+		var item = $(this.el).detach();
+		item.removeClass('tile');
+		item.css({
+			height: 64,
+			width : 64,
+			overflow : 'hidden',
+			top : 0,
+			left : 0
+		})
+		$('.wishList').append(item);
+		$('.mainWrapper').masonry('reload');
+	},
+	addToWishList : function(){
+		
 	}
 });
 $(function(){
